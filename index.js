556b258543612caa93a5a86561af561ce47c513e
Esra Ayaz <essayaz@hotmail.com>
2025-06-13 22:19:15 +0200

    Project finished.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Selectors
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 // Functions
 const generateTemplate = (todo) => {
@@ -13,6 +14,16 @@ const generateTemplate = (todo) => {
     </ul>
     `;
   list.innerHTML += html;
+};
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
 };
 
 // Event Listeners
@@ -30,4 +41,9 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
