53791c772e2fd81b48dc1b2cdc5dae1471068d11
Esra Ayaz <essayaz@hotmail.com>
2025-06-13 11:47:51 +0200

    JS started

---
+++ index.js
@@ -0,0 +1,26 @@
+// Selectors
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+  const html = `
+    <ul class="list-group todos">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="fa-solid fa-trash delete"></i>
+        </li>
+    </ul>
+    `;
+  list.innerHTML += html;
+};
+
+// Event Listeners
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
